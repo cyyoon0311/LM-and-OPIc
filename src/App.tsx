5637d668b83a9a3ffe38594420e23d2952f395c1
cyyoon0311 <cy_0311@naver.com>
2026-02-26 01:32:25 +0900

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import HomeScreen from './screens/HomeScreen'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-[#f0f0f0] flex items-center justify-center p-6">
+      <HomeScreen />
+    </div>
+  )
+}
